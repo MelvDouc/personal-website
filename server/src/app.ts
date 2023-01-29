aaeec319c6257244a3d571f13e64849c295bbc24
MelvDouc <melv.douc@gmail.com>
2023-01-30 00:04:55 +0100

    Create client route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,6 @@
 import cors from "cors";
 import express from "express";
 import router from "./routes/router.js";
-import "./database/db.js";
 
 const port = process.env.PORT ?? process.env.port ?? process.env.Port ?? 10_002;
 const app = express();
@@ -9,6 +8,7 @@ const app = express();
 app.use(cors({
   origin: process.env.CLIENT_ORIGIN
 }));
+// app.use(express.static("client/dist"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,19 +1,24 @@
-import { Router } from "express";
+import { Router, static as expressStatic } from "express";
+import { readFile } from "fs/promises";
+import { join, resolve } from "path";
 import vocController from "../controllers/voc.controller.js";
 import entityId from "../middleware/entity-id.middleware.js";
 
 const router = Router();
-
-router.get("/", (req, res) => {
-  res.json({
-    hello: "world!"
-  });
-});
+const clientDir = resolve("client", "dist");
 
 router.get("/voc/english/words/get-one", entityId, vocController.englishWordsCrud.getOne);
 router.get("/voc/english/words/get-all", vocController.englishWordsCrud.getAll);
 router.post("/voc/english/words/create", vocController.englishWordsCrud.create);
 router.put("/voc/english/words/update", entityId, vocController.englishWordsCrud.update);
 router.delete("/voc/english/words/delete", entityId, vocController.englishWordsCrud.delete);
+
+router.get("/", async (req, res) => {
+  const htmlPath = join(clientDir, "index.html");
+  const data = await readFile(htmlPath);
+  req.app.use(expressStatic(clientDir));
+  res.end(data);
+});
+
 
 export default router;
