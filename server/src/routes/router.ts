3db5c371f504f07327b6237cfc86a900492f16da
MelvDouc <melv.douc@gmail.com>
2023-01-30 00:56:41 +0100

    index on main: aaeec31 Create client route

--- server/src/routes/router.ts
+++ server/src/routes/router.ts
@@ -1,11 +1,11 @@
 import { Router, static as expressStatic } from "express";
 import { readFile } from "fs/promises";
-import { join, resolve } from "path";
+import { join } from "path";
 import vocController from "../controllers/voc.controller.js";
 import entityId from "../middleware/entity-id.middleware.js";
 
 const router = Router();
-const clientDir = resolve("client", "dist");
+const clientDir = join(process.cwd(), "client", "dist");
 
 router.get("/voc/english/words/get-one", entityId, vocController.englishWordsCrud.getOne);
 router.get("/voc/english/words/get-all", vocController.englishWordsCrud.getAll);
@@ -14,10 +14,14 @@ router.put("/voc/english/words/update", entityId, vocController.englishWordsCrud
 router.delete("/voc/english/words/delete", entityId, vocController.englishWordsCrud.delete);
 
 router.get("/", async (req, res) => {
-  const htmlPath = join(clientDir, "index.html");
-  const data = await readFile(htmlPath);
-  req.app.use(expressStatic(clientDir));
-  res.end(data);
+  try {
+    const htmlPath = join(clientDir, "index.html");
+    const data = await readFile(htmlPath);
+    req.app.use(expressStatic(clientDir));
+    res.end(data);
+  } catch (error) {
+    res.end((<Error>error).message);
+  }
 });
 
 
