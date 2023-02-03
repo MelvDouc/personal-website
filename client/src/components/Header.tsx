c07691a85bd582c0b15c1014eb6b03e0995b516d
MelvDouc <melv.douc@gmail.com>
2023-02-04 00:32:48 +0100

    Use BS columns

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,18 +4,20 @@ import "./Header.scss";
 export default function Header() {
   return (
     <header className="container-xxl py-1 text-light bg-green1">
-      <div className="container d-flex justify-content-between">
-        <article className="header-block">
-          <h1 className="fs-2 m-0">Melvin Doucet</h1>
-        </article>
-        <article className="header-block">
-          <nav className="h-100">
-            <ul className="list-style-none d-flex align-items-center gap-3 h-100 p-0 m-0">
-              <li><Link href="/">Home</Link></li>
-              <li><Link href="/test">Test</Link></li>
-            </ul>
-          </nav>
-        </article>
+      <div className="container">
+        <div className="row">
+          <article className="col">
+            <h1 className="fs-2 m-0">Melvin Doucet</h1>
+          </article>
+          <article className="col d-flex justify-content-center">
+            <nav className="h-100">
+              <ul className="list-style-none d-flex align-items-center gap-3 h-100 p-0 m-0">
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/test">Test</Link></li>
+              </ul>
+            </nav>
+          </article>
+        </div>
       </div>
     </header>
   );
