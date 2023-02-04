0aecb93bb8d4461ee3c4be4f65faff811e5db5f6
MelvDouc <melv.douc@gmail.com>
2023-02-04 22:44:33 +0100

    And client env

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -3,7 +3,21 @@
 type OptionalPromise<T> = T | Promise<T>;
 type Obs<T> = import("reactfree-jsx").Observable<T>;
 
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
 interface Page {
   title?: string;
   component: () => OptionalPromise<Node | string>;
+}
+
+interface EmailData {
+  email: string;
+  subject: string;
+  message: string;
 }
