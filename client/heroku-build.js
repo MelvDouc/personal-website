e6de38fb6ed2783e91c4975b00072ab5bae553fd
MelvDouc <melv.douc@gmail.com>
2023-02-04 14:33:54 +0100

    Create heroku-build script

---
+++ client/heroku-build.js
@@ -0,0 +1,16 @@
+import { exec } from "child_process";
+import { readdir } from "fs/promises";
+import { resolve, join } from "path";
+import { promisify } from "util";
+
+const run = promisify(exec);
+const root = resolve(".");
+const dirs = await readdir(join(root, "client"));
+
+if (!dirs.includes("node_modules")) {
+  console.log("Installing client/node_modules...");
+  await run("npm --prefix client install");
+}
+
+await run("npm --prefix client run build && rm -rf client/node_modules");
+console.log("Client was successfully built.");
