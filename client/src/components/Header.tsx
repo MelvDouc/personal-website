9f5c023341248a92214970bd60ed0a3ce770332d
MelvDouc <melv.douc@gmail.com>
2023-02-12 02:06:02 +0100

    Update header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,23 +3,62 @@ import "./Header.scss";
 
 export default function Header() {
   return (
-    <header className="py-1 text-light bg-primary">
-      <div className="container">
-        <div className="row">
-          <article className="col">
-            <h1 className="fs-2 m-0">Melvin Doucet</h1>
-          </article>
-          <article className="col d-flex justify-content-center">
-            <nav className="h-100">
-              <ul className="list-style-none d-flex align-items-center gap-3 h-100 p-0 m-0">
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/projects">Projects</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
-              </ul>
-            </nav>
-          </article>
+    <header className="bg-primary">
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <h1 className="m-0 d-flex align-items-center">
+            <Link className="navbar-brand" href="/">
+              Melvin Doucet
+            </Link>
+          </h1>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link href="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  href="/projects"
+                  className="nav-link dropdown-toggle"
+                  role="button"
+                  $init={a => {
+                    a.dataset.bsToggle = "dropdown";
+                    a.setAttribute("aria-expanded", "false");
+                  }}
+                >
+                  Projects
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <Link href="/projects/calculator" className="dropdown-item">
+                      Calculator
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li className="nav-item">
+                <Link href="/contact" className="nav-link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </nav>
     </header>
   );
 }
--- client/src/components/Link.tsx
+++ client/src/components/Link.tsx
@@ -1,14 +1,37 @@
 import router from "../routing/router.js";
 
-export default function Link({ href, children }: {
+export default function Link({
+  href,
+  className,
+  attributes,
+  children
+}: {
   href: string;
+  className?: string;
+  attributes?: Record<string, string>;
   children?: any;
 }) {
   return (
-    <a href={href} onclick={(e) => {
-      e.preventDefault();
-      history.pushState({}, "", href);
-      router.setUrl(href);
-    }}>{children}</a>
+    <a
+      href={href}
+      $init={element => {
+        element.addEventListener("click", handleClick(href, router));
+        if (className) element.className = className;
+        if (attributes)
+          Object.entries(attributes).forEach(([name, value]) => {
+            element.setAttribute(name, value);
+          });
+      }}
+    >
+      {children}
+    </a>
   );
+}
+
+function handleClick(href: string, router: { setUrl: (url: string) => void }) {
+  return (e: Event) => {
+    e.preventDefault();
+    history.pushState({}, "", href);
+    router.setUrl(href);
+  };
 }
