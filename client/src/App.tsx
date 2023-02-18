92e87fe335be9e66a5e093701d664164b64ad077
MelvDouc <melv.douc@gmail.com>
2023-02-18 22:57:35 +0100

    Use fragment syntax

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,17 @@
 import "./App.scss";
 import Header from "./components/Header/Header.jsx";
-import Main from "./components/Main.jsx";
+import Main from "./components/Main/Main.jsx";
 import router from "./routing/router.js";
 
 export default function App() {
   window.addEventListener("popstate", () => router.setUrl(location.pathname + location.search));
   router.onUrlChange(({ title }) => document.title = `Melvin Doucet's Website | ${title ?? "?"}`);
 
-  const app = document.createDocumentFragment();
-  app.append(
-    <Header />,
-    <Main router={router} />
+  const app = (
+    <>
+      <Header />
+      <Main router={router} />
+    </>
   );
 
   router.setUrl(location.pathname + location.search);
--- client/src/pages/ContactPage.tsx
+++ client/src/pages/ContactPage.tsx
@@ -1,7 +1,10 @@
 import ContactForm from "../components/ContactForm.jsx";
 
 export default function ContactPage() {
-  const fragment = document.createDocumentFragment();
-  fragment.append(<h2>Contact</h2>, <ContactForm />);
-  return fragment;
+  return (
+    <>
+      <h2>Contact</h2>
+      <ContactForm />
+    </>
+  );
 }
