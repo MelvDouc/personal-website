a62bd783cf99871847c469be12a5c7e7c8271253
MelvDouc <melv.douc@gmail.com>
2023-02-18 23:57:07 +0100

    Update className

--- client/src/components/SmallComponentWrapper.tsx
+++ client/src/components/SmallComponentWrapper.tsx
@@ -1,6 +1,6 @@
 export default function SmallComponentWrapper({ children }: { children?: any; }) {
   return (
-    <div className="container h-100 d-flex align-items-center justify-content-center">
+    <div className="h-100 d-flex align-items-center justify-content-center">
       {children}
     </div>
   );
