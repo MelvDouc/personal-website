1c009685af591d2dc3441e9eb6697d665a641ca0
MelvDouc <melv.douc@gmail.com>
2023-02-18 22:58:12 +0100

    Disuse bootstrap

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -4,59 +4,34 @@ import "./Header.scss";
 
 export default function Header() {
   return (
-    <header className="bg-primary">
-      <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid">
-          <h1 className="m-0 d-flex align-items-center">
-            <Link className="navbar-brand" href={urls.HOME.url}>
-              Melvin Doucet
-            </Link>
-          </h1>
-          <button
-            className="navbar-toggler border-white"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link href={urls.HOME.url} className="nav-link">
-                  Home
-                </Link>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  href={urls.PROJECTS.url}
-                  className="nav-link dropdown-toggle"
-                  aria-expanded="false"
-                  data-bs-toggle="dropdown"
-                >
-                  Projects
-                </a>
-                <ul className="dropdown-menu">
-                  {Object.values(projects).map(({ url, title }) => (
-                    <li>
-                      <Link href={url} className="dropdown-item">{title}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Link href={urls.CONTACT.url} className="nav-link">
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <header className="d-flex gap-6">
+      <section>
+        <h1>
+          <Link href={urls.HOME.url}>Melvin Doucet</Link>
+        </h1>
+      </section>
+      <section>
+        <nav>
+          <ul className="list-style-none d-flex gap-4 w-100 h-100">
+            <li>
+              <Link href={urls.HOME.url}>Home</Link>
+            </li>
+            <li>
+              <div className="grid-center">Projects</div>
+              <ul className="d-none">
+                {Object.values(projects).map(({ url, title }) => (
+                  <li>
+                    <Link href={url}>{title}</Link>
+                  </li>
+                ))}
+              </ul>
+            </li>
+            <li>
+              <Link href={urls.CONTACT.url}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      </section>
     </header>
   );
 }
---
+++ client/src/components/Main/Main.tsx
@@ -0,0 +1,14 @@
+import type { Router } from "../../routing/router.js";
+
+export default function Main({ router }: { router: Router; }) {
+  return (
+    <main
+      className="p-4"
+      $init={element => {
+        router.onUrlChange(async ({ component }) => {
+          element.replaceChildren(await component());
+        });
+      }}
+    ></main>
+  );
+}
