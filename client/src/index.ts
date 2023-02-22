4809b9822ffd8a68b78e4c61491d00591eb8a308
MelvDouc <melv.douc@gmail.com>
2023-02-22 22:25:49 +0100

    minor

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,4 +1,4 @@
-import App from "./App.jsx";
+import App from "./App.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   document.body.append(App());
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -11,7 +11,9 @@ export default function HomePage() {
         <p className="px-3 py-2 border-rounded bg-dark-transparent">I live in Thionville, Northeastern France, near Luxembourg. I've been a web developer for about two years. Though I've worked with many technologies like Java and PHP, I specialize in full-stack development with the ever popular JavaScript language. Need a fast, modern, versatile, scalable website? You're in the right place.</p>
       </section>
       <section>
-        <Link href={urls.CONTACT.url} className="fs-5 btn btn-primary">Contact</Link>
+        <div className="grid-center">
+          <Link href={urls.CONTACT.url} className="fs-5 btn btn-primary">Contact</Link>
+        </div>
       </section>
     </div>
   );
