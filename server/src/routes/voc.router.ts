36a929e74785b7c897dab091e3ca39658ff311ff
MelvDouc <melv.douc@gmail.com>
2023-02-23 17:57:41 +0100

    white space

--- server/src/routes/voc.router.ts
+++ server/src/routes/voc.router.ts
@@ -3,25 +3,10 @@ import { englishVocController } from "../controllers/voc.controller.js";
 
 const vocRouter = Router();
 
-vocRouter.get(
-  "/one/:id",
-  async (req, res) => await englishVocController.getOne(req, res)
-);
-vocRouter.get(
-  "/all",
-  async (req, res) => await englishVocController.getAll(req, res)
-);
-vocRouter.post(
-  "/create",
-  async (req, res) => await englishVocController.create(req, res)
-);
-vocRouter.patch(
-  "/update/:id",
-  async (req, res) => await englishVocController.update(req, res)
-);
-vocRouter.delete(
-  "/delete/:id",
-  async (req, res) => await englishVocController.delete(req, res)
-);
+vocRouter.get("/one/:id", async (req, res) => await englishVocController.getOne(req, res));
+vocRouter.get("/all", async (req, res) => await englishVocController.getAll(req, res));
+vocRouter.post("/create", async (req, res) => await englishVocController.create(req, res));
+vocRouter.patch("/update/:id", async (req, res) => await englishVocController.update(req, res));
+vocRouter.delete("/delete/:id", async (req, res) => await englishVocController.delete(req, res));
 
 export default vocRouter;
