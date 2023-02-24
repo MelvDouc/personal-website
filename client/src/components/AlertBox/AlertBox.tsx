3b707e3a8141a44db67c689e5a0acccf435253f6
MelvDouc <melv.douc@gmail.com>
2023-02-24 10:57:25 +0100

    add keydown

--- client/src/components/AlertBox/AlertBox.tsx
+++ client/src/components/AlertBox/AlertBox.tsx
@@ -9,31 +9,36 @@ export default function displayAlterBox(props: Parameters<typeof AlertBox>[0]) {
   );
 }
 
-function AlertBox({
-  message,
-  handleClose
-}: {
+function AlertBox({ message, handleClose }: {
   message: string;
   /**
    * Will run after the element has been removed.
    */
   handleClose?: VoidFunction;
 }) {
-  let remove: VoidFunction;
-  const quit = (): void => {
-    remove();
+  let alertBoxContainer: HTMLElement;
+  const quit = () => {
+    alertBoxContainer.remove();
     handleClose && handleClose();
   };
 
   return (
-    <div
-      className={classes.alertBox}
-      $init={element => remove = () => element.remove()}
-    >
-      <form>
+    <div className={classes.alertBoxContainer} $init={(element) => {
+      alertBoxContainer = element;
+      document.addEventListener("keydown", (e: KeyboardEvent) => {
+        if (e.key !== "Enter") return;
+        e.preventDefault();
+        quit();
+      }, { once: true });
+    }}>
+      <div className={classes.alertBox}>
         <p>{message}</p>
-        <button className="btn btn-primary" onclick={quit}>OK</button>
-      </form>
+        <button
+          className="btn btn-primary"
+          type="button"
+          onclick={quit}
+        >OK</button>
+      </div>
     </div>
   );
 }
