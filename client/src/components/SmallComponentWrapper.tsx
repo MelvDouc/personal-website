bd5037515b124a55a0921dc9a710c2086a643f1c
MelvDouc <melv.douc@gmail.com>
2023-02-25 23:21:48 +0100

    fix home page

--- client/src/components/SmallComponentWrapper.tsx
+++ client/src/components/SmallComponentWrapper.tsx
@@ -1,6 +1,6 @@
 export default function SmallComponentWrapper({ children }: { children?: any; }) {
   return (
-    <div className="h-100 p-2 d-flex flex-column align-items-center justify-content-center">
+    <div className="small-component-wrapper">
       {children}
     </div>
   );
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -3,13 +3,13 @@ import urls from "../routing/urls.js";
 
 export default function HomePage() {
   return (
-    <div className="page text-light">
+    <div className="page page-lighter">
       <section>
         <h1 className="fs-9">A Website Bespoke And Exactly Your Size</h1>
       </section>
       <section className="fs-5">
-        <div className="d-flex flex-column gap-2 px-3 py-2 border-rounded">
-          <p>I'm a web developer based near Luxembourg City. I have worked with many technologies including Java and PHP. However, my passion lies in full-stack development with the versatile and ever-popular JavaScript language.</p>
+        <div>
+          <p className="mb-2">I'm a web developer based near Luxembourg City. I have worked with many technologies including Java and PHP. However, my passion lies in full-stack development with the versatile and ever-popular JavaScript language.</p>
           <p>My expertise allows me to create fast, modern, and scalable websites that are tailored to your needs. Whether you're looking for a sleek and minimalistic design, or a complex and dynamic web application, I will deliver high-quality results. If you're looking for a reliable and competent web dev, you've come to the right place. Together we'll bring your ideas to life!</p>
         </div>
       </section>
