8cbe890129e24d05c089168652413169c1ff321e
MelvDouc <melv.douc@gmail.com>
2023-03-03 20:51:32 +0100

    update syntax

--- client/src/components/Link.tsx
+++ client/src/components/Link.tsx
@@ -1,12 +1,23 @@
 import router from "../routing/router.js";
 
-export default function Link(props: JSX.IntrinsicElements["a"] & Required<Pick<JSX.IntrinsicElements["a"], "href">> & { children?: any; }) {
-  const { children, ...otherProps } = props;
-  otherProps.onclick = (e) => {
-    e.preventDefault();
-    history.pushState({}, "", (e.target as HTMLAnchorElement).href);
-    router.setUrl(props.href as string);
-  };
-
-  return h("a", otherProps, ...(children ?? [])) as HTMLAnchorElement;
+export default function Link({ href, className, $init, children }: {
+  href: string;
+  className?: string;
+  $init?: (a: HTMLAnchorElement) => void;
+  children?: any;
+}) {
+  return (
+    <a
+      href={href}
+      onclick={(e) => {
+        e.preventDefault();
+        history.pushState({}, "", (e.target as HTMLAnchorElement).href);
+        router.setUrl(href);
+      }}
+      $init={(a) => {
+        className && (a.className = className);
+        $init && $init(a);
+      }}
+    >{children}</a>
+  );
 }
