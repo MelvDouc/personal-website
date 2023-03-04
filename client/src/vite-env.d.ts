480f2edb010e3a0d3cdadfbc8cfae19e20f14c97
MelvDouc <melv.douc@gmail.com>
2023-03-04 22:02:54 +0100

    Upgrades

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -3,7 +3,7 @@
 declare function h(tagName: string, props: Record<string, any>, ...children: any[]): any;
 
 type OptionalPromise<T> = T | Promise<T>;
-type Obs<T> = import("reactfree-jsx").Observable<T>;
+type Obs<T> = Observable<T>;
 
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
