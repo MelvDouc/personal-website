dcece96876b3a5d0f3d29d210cba8b7e8bf65fce
MelvDouc <melv.douc@gmail.com>
2023-03-13 15:14:14 +0100

    syntax #2

--- client/heroku-build.sh
+++ client/heroku-build.sh
@@ -1,8 +1,6 @@
-cd client
-[ -d "$PWD/node_modules" ] && echo "client/node_modules already installed." \
+[ -d "$PWD/client/node_modules" ] && echo "client/node_modules already installed." \
 || echo "Installing client/node_modules..." && npm install
 npm run build
 echo "Removing dev folders and files..."
-rm -rf !(dist|heroku-build.sh)
+rm -rf client/!(dist|heroku-build.sh)
 echo "Client was successfully installed."
-cd ..
