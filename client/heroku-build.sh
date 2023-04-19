946ca871c446998ac74d34354fd905a268dfa02e
MelvDouc <melv.douc@gmail.com>
2023-04-19 19:38:46 +0200

    Update build script

--- client/heroku-build.sh
+++ client/heroku-build.sh
@@ -1,6 +1,8 @@
-[ -d "$PWD/client/node_modules" ] && echo "client/node_modules already installed." \
+cd client
+[ -d "$PWD/node_modules" ] && echo "client/node_modules already installed." \
 || echo "Installing client/node_modules..." && npm install
 npm run build
 echo "Removing dev folders and files..."
 rm -rf client/!(dist|heroku-build.sh)
 echo "Client was successfully installed."
+cd ..
